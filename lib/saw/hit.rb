ce396faafa0f914417424eef13c9ddecc8b1f57a
Amol Pujari <amolpujari@gmail.com>
2013-04-02 09:39:05 +0530

    minor

--- lib/saw/hit.rb
+++ lib/saw/hit.rb
@@ -1,6 +1,7 @@
 module Saw
   class Hit < ActiveRecord::Base
     default_scope order('id')
+    attr_accessible :url, :http_method, :action, :params, :note, :json_data
     belongs_to :visit
   end
 end

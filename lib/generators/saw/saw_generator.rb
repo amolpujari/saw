033505b09bbc422c8a20d2270e4ef391d7841802
Amol Pujari <amolpujari@gmail.com>
2013-04-02 09:24:20 +0530

    working out generators

---
+++ lib/generators/saw/saw_generator.rb
@@ -0,0 +1,22 @@
+require 'rails/generators/active_record'
+
+class SawGenerator < ActiveRecord::Generators::Base
+  include Rails::Generators::ResourceHelpers
+
+  desc "Create a migration to add saw-specific data. " +
+       "Add tables visits and hits." +
+       "Adds a route for recording visits by POST ajax" +
+       "Adds POST /visits => visits#create"
+
+  def self.source_root
+    @source_root ||= File.expand_path('../templates', __FILE__)
+  end
+
+  def generate_migration
+    migration_template "saw_migration.rb.erb", "db/migrate/add_visits_and_hits}"
+  end
+
+  def generate_routes
+    route 'post "visits" => "visits#create", :defaults => { :format => "json"}'
+  end
+end

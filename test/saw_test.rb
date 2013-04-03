6496f3e90080f36569c5020161c8a002e35673ab
Amol Pujari <amolpujari@gmail.com>
2013-04-03 14:44:31 +0530

    preparing for tests

---
+++ test/saw_test.rb
@@ -0,0 +1,18 @@
+$:.unshift(File.dirname(__FILE__))
+
+require 'helpers/unit_test_helper'
+require 'models/user'
+require 'models/visit'
+require 'models/hit'
+
+ActionController::Routing::Routes.draw do |map|
+  post "visits" => "visits#create"#, :defaults => { :format => "json"}
+end
+
+class SawTests < Test::Unit::TestCase  # :nodoc:
+  fixtures :users, :visits, :hits
+
+  def setup
+  end
+
+end

e2b93a4885ff374d25490404a4d5397f1dcff9a4
Amol Pujari <amolpujari@gmail.com>
2013-04-03 13:48:08 +0530

    minor

--- examples/active_admin/saw.rb
+++ examples/active_admin/saw.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Visit do
   menu :parent => "Users"
   belongs_to :user, :optional => true
 
-  actions :index, :show, :delete
+  actions :all, :except => [:edit, :update, :create]
 
   index do
     selectable_column
@@ -25,12 +25,7 @@ ActiveAdmin.register Visit do
       resource.lasts
     end
 
-    column "" do |resource|
-      links = ''
-      links << (link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link")
-      links << (link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link")
-      links.html_safe
-    end
+    default_actions
   end
 end
 
@@ -38,7 +33,7 @@ ActiveAdmin.register Hit do
   menu :parent => "Users"
   belongs_to :visit, :optional => true
 
-  actions :index, :show, :delete
+  actions :all, :except => [:edit, :update, :create]
 
   index do
     selectable_column
@@ -48,11 +43,6 @@ ActiveAdmin.register Hit do
       "#{resource.http_method} #{resource.action}"
     end 
 
-    column "" do |resource|
-      links = ''
-      links << (link_to I18n.t('active_admin.view'), resource_path(resource), :class => "member_link view_link")
-      links << (link_to I18n.t('active_admin.delete'), resource_path(resource), :method => :delete, :confirm => I18n.t('active_admin.delete_confirmation'), :class => "member_link delete_link")
-      links.html_safe
-    end
+    default_actions
   end
 end

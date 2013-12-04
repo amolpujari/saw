e817fbabc8ec6d5ac1330a4c71d62090be1702c2
Amol Pujari <amol@plotwatt.com>
2013-12-04 15:26:15 +0530

    minor

--- lib/saw/hit.rb
+++ lib/saw/hit.rb
@@ -2,6 +2,7 @@ class Hit < ActiveRecord::Base
   serialize :json_data, JSON  
   attr_accessible :url, :http_method, :action, :params, :visit_id, :note, :json_data
   belongs_to :visit
+  delegate :user, :to => :visit
 
   def last_hit
     visit.hits.where(' hits.id < ? ', id).last
@@ -13,13 +14,5 @@ class Hit < ActiveRecord::Base
 
   def after
     Saw::Util.time_diff visit.starts_with.created_at, created_at
-  end
-
-  def user
-    visit and visit.user
-  end
-
-  def user_visits
-    user and user.visits
   end
 end
